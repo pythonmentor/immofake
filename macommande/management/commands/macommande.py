740801a5f7c407574f6c8884586af63fd65b9552
Thierry Chappuis <thierry@chappuis.io>
2019-06-27 06:57:41 +0200

    Modified macommande

--- macommande/management/commands/macommande.py
+++ macommande/management/commands/macommande.py
@@ -1,6 +1,12 @@
+import os 
+
 from django.core.management.base import BaseCommand
 from sentry_sdk import capture_message
 
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        capture_message("macommande called")
+        capture_message(
+            "macommande called, IMMOFAKE has value "
+            f"{os.environ.get('IMMOFAKE', 'no value')}"
+        )
